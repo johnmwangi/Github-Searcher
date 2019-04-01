e52e823165675d943c71b89324792a1f64eeffe2
johnmwangi <jonesmwas356@gmail.com>
2019-04-01 13:59:10 +0300

    app component.ts changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,1 +1,11 @@
+import { Component } from '@angular/core';
+// import { SearchService } from "./git-search/search.service";
 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Github-search';
+}
